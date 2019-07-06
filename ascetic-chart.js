ea6b2514ab3add009d5887e44933956c5537436c
nik <nik@0x4e494b.ru>
2019-07-06 23:12:42 +0300

    change logic in draw charts

--- ascetic-chart.js
+++ ascetic-chart.js
@@ -1,25 +1,42 @@
 import _ from 'lodash';
 
  const  Chart = function(){
-      const recFabric=function(ctx, arrData){
-      const dy = parseInt(arrData.height);
-      const wdh = parseInt(arrData.width);
+      let deltaX = 0;
+      let deltaY = 0;
+    
+    const recFabricRec=function(ctx, arrData){
+      
+      const imgHeight = parseInt(arrData.height);
+      const imgWidth = parseInt(arrData.width);
+
       const chwdh = parseInt(arrData['chart-width']);
+      const chhgt = parseInt(arrData['chart-height']);
+
+      const gap = parseInt(arrData['chart-gap']);
+
       const xdim = parseInt(arrData['x-dim']);
       const ydim = parseInt(arrData['y-dim']);
+
+      const H = chhgt/ydim;
+      const W = chwdh/xdim;
+      
+      console.warn(arrData);
       
       var numCh = _.size(arrData.aD);
       
       _.forEach(arrData.aD,function(el){
       
          ctx.fillStyle = el.color;
-         // var leftCornerX = el.index*xdim;
-         var leftCornerX = el.index*chwdh;
-         var leftCornerY = dy - el.height*ydim; //fixme
-         var drawWidth = chwdh;
-         var drawHeight = el.height*ydim; //fixme
+      
+         var leftCornerX = (el.index-1)*chwdh + gap*el.index;
+         var leftCornerY = imgHeight - (H*el.height/100)*ydim; //fixme
+         var drawWidth = chwdh; //fixme
+         var drawHeight = (H*el.height/100)*ydim; //fixme
+
          ctx.fillRect(leftCornerX,leftCornerY,drawWidth,drawHeight); 
+
          console.log(leftCornerX,leftCornerY,drawWidth,drawHeight);
+         
          return;
       
       });         
@@ -27,10 +44,24 @@ import _ from 'lodash';
       return ctx;        
      }
 
+    const recFabricGrid = function(ctx){
+
+      //!
+      return ctx;
+    } 
+
+    const recFabricAxis = function(ctx){
+
+      //!
+      return ctx;
+     }
+
+
      return function(arr){
       console.log("in module AsceticChart, function Run!!!");
       console.log(arr);
         var obj=document.getElementById(arr.idName);
+        console.log(obj);
         const cvs = document.createElement('canvas');
         cvs.className = "ascetic-chart";
         cvs.width = arr.width;
@@ -38,7 +69,7 @@ import _ from 'lodash';
         cvs.style.backgroundColor=arr['background-color'];
         obj.append(cvs);
         var ctx = cvs.getContext("2d");
-        recFabric(ctx, arr);
+        recFabricRec(ctx, arr);
      }     
    }
 export {Chart};
